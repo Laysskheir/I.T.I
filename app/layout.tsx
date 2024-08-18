57900a92f3dcb2b7373a8ffb8eaf5955e25869ca
Layss kheir <laysskheir@gmail.com>
2024-08-18 18:15:28 +0300

    add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { siteConfig } from "@/config/site";
 import { Poppins } from "next/font/google";
 import JsonLd from "@/components/JsonLd";
+import { Analytics } from "@vercel/analytics/react"
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -46,6 +47,7 @@ export default async function RootLayout({
       >
         <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]" />
         {children}
+        <Analytics/>
       </body>
     </html>
   );
